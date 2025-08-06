ffefc5d23b5bebc40a8f6e001352a6f78f4647ba
LuisMontenegro21 <83621697+LuisMontenegro21@users.noreply.github.com>
2025-08-05 21:16:16 -0600

    Update hamming_encryptor.c

--- hamming_encryptor.c
+++ hamming_encryptor.c
@@ -2,25 +2,74 @@
 #include <stdlib.h>
 #include <stdbool.h>
 #include <string.h>
+//#include <math.h>
+
+
+// check powers
+enum Powers {
+    p1 = 1,
+    p2 = 2,
+    p3 = 4,
+    p4 = 8,
+    p5 = 16,
+    p6 = 32,
+    p7 = 64,
+    p8 = 128,
+    p9 = 256,
+    p10 = 512,
+    p11 = 1024,
+    p12 = 2048
+};
 
 /*
- Runs the hamming code to check for errors in a message
+Auxiliary functions
 */
+void reverse(char *bin, int left, int right) {
+    while (left < right) {
+        char temp = bin[left];
+        bin[left] = bin[right];
+        bin[right] = temp;
+        left++;
+        right--;
+    }
+}
+
+char* to_binary(int n) {
+    int index = 0;
+	char* bin = (char*) malloc(32 * sizeof(char));
+
+    while (n > 0) {
+        int bit = n % 2;
+        bin[index++] = '0' + bit;
+        n /= 2;
+    }
+    bin[index] = '\0';
+
+	reverse(bin, 0, index-1);
+  	return bin;
+}
+
 bool hamming_code(const char *message, int size){
-    for (int i = 0; i!=size; i++){
-        // printf("i: %d\n", i);
-        int val = message[i] - '0'; // turns ascii value into 1 and 0
-        printf("Index: %d Value: %d\n", i+1, val);
+
+    for (int index = 0; index!=size; index++){
+        char* bin = to_binary(index); // returns a list of binary
+        int index_value = message[index] - '0'; // turns ascii value into 1 and 0 
+        printf("Index value: %d | Binary index: %s\n", index_value, bin);
     }
     return true;
 }
+
 
 /*
  Checks that all symbols are 0 and 1
 */
 bool check_symbols(const char *message, int size){
     for (int i = 0; i!=size; i++){
-        if (message[i] - '0' != 0 || message[i] - '0' != 1) return false;
+        int val = message[i] - '0';
+        if (val != 0 || val != 1){
+            printf("Message error: %d\n", val);
+            return false;
+        }
     }
     return true;
 }
@@ -32,6 +81,7 @@ int main(int argc, char *argv[]){
 
     char *message = NULL;
     size_t initial_size = 100;
+    int minimal_size = 4;
 
     message = (char *)malloc(initial_size * sizeof(char)); // allocate memory
     if (message == NULL){
@@ -44,6 +94,12 @@ int main(int argc, char *argv[]){
     }
 
     size_t real_length = strlen(message); // get real size
+    // check minimal size allowed 
+    if ((int)real_length < minimal_size){
+        printf("Error: Minimal length for a code is 4: %d found\n", (int)real_length);
+        return 1;
+    }
+    // reallocate memory to fit the size
     char *temp_string = (char *)realloc(message, (real_length + 1) * sizeof(char));
     if (temp_string == NULL){
         perror("Failed to realocate memory");
@@ -52,13 +108,15 @@ int main(int argc, char *argv[]){
     }
     message = temp_string;
     int size = (int)real_length;
-    if(!check_symbols(message, size)){
-        printf("Unknown symbol found on message: only 0 and 1 accepted");
-        return 1;
-    }
+    // check data integrity
+    // if(check_symbols(message, size) == false){
+    //     printf("Unknown symbol found on message: only 0 and 1 accepted");
+    //     return 1;
+    // }
+    // perform hamming code
     if(hamming_code(message, size)){
         printf("Message has no errors");
-    } // perform hamming code
+    } 
 
     free(message);
     return 0;
